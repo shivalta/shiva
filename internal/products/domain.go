08b90494fa2dbeff6d2f73ace033c6202ba5bdf3
aprian1337 <dwikyapriyan@gmail.com>
2022-01-23 02:00:27 +0700

    add: products domain

---
+++ internal/products/domain.go
@@ -0,0 +1,54 @@
+package products
+
+import "mime/multipart"
+
+type Domain struct {
+	ID                uint
+	ProductClassId    uint
+	ProductClass      Class
+	ProductCategoryId uint
+	ProductCategory   Categories
+	Sku               string
+	Name              string
+	AdminFee          int
+	Stock             int
+	Price             int
+	IsActive          bool
+}
+
+type Class struct {
+	ID          uint
+	Name        string
+	IsPasca     bool
+	ImageUrl    string
+	ImageHeader *multipart.FileHeader
+	Slug        string
+}
+
+type Categories struct {
+	ID             uint
+	ProductClassId uint
+	ProductClass   Class
+	Name           string
+	ImageUrl       string
+	ImageHeader    *multipart.FileHeader
+	Slug           string
+	Tax            float32
+}
+
+type Usecase interface {
+	GetAll(search string, key string) ([]Domain, error)
+	GetById(id uint) (Domain, error)
+	Create(class Domain) (Domain, error)
+	Update(class Domain) (Domain, error)
+	Delete(id uint) error
+}
+
+type Repository interface {
+	GetAll(search string, key string) ([]Domain, error)
+	GetById(id uint) (Domain, error)
+	Create(class Domain) (Domain, error)
+	Update(class Domain) (Domain, error)
+	UpdateWithoutImage(class Domain) (Domain, error)
+	Delete(id uint) error
+}
--- internal/products/repository/model.go
+++ internal/products/repository/model.go
@@ -1,43 +1,58 @@
 package repository
 
-//
-//import (
-//	"gorm.io/gorm"
-//	"shiva/shiva-auth/internal/class"
-//	"strings"
-//	"time"
-//)
-//
-//type ProductClass struct {
-//	gorm.Model
-//	Name      string    `gorm:"omitempty"`
-//	IsPasca   bool      `gorm:"omitempty"`
-//	Image     string    `gorm:"omitempty"`
-//	CreatedAt time.Time `gorm:"autoCreateTime"`
-//	UpdatedAt time.Time `gorm:"autoUpdateTime"`
-//}
-//
-//func FromDomain(u class.Domain) ProductClass {
-//	return ProductClass{
-//		Name:    u.Name,
-//		IsPasca: u.IsPasca,
-//		Image:   u.ImageUrl,
-//	}
-//}
-//
-//func (u *ProductClass) ToDomain() class.Domain {
-//	return class.Domain{
-//		ID:       u.ID,
-//		Name:     u.Name,
-//		IsPasca:  u.IsPasca,
-//		ImageUrl: u.Image,
-//		Slug:     strings.ToLower(strings.ReplaceAll(u.Name, " ", "")),
-//	}
-//}
-//
-//func ToDomainList(user []ProductClass) (list []class.Domain) {
-//	for _, v := range user {
-//		list = append(list, v.ToDomain())
-//	}
-//	return list
-//}
+import (
+	"gorm.io/gorm"
+	rcat "shiva/shiva-auth/internal/categories/repository"
+	rclass "shiva/shiva-auth/internal/class/repository"
+	"shiva/shiva-auth/internal/products"
+	"time"
+)
+
+type Products struct {
+	gorm.Model
+	ProductClassId    uint
+	ProductClass      rclass.ProductClass `gorm:"foreignKey:ProductClassId"`
+	ProductCategoryId uint
+	ProductCategory   rcat.ProductCategories `gorm:"foreignKey:ProductCategoryId"`
+	Sku               string
+	Name              string
+	AdminFee          int
+	Stock             int
+	Price             int
+	IsActive          bool
+	CreatedAt         time.Time `gorm:"autoCreateTime"`
+	UpdatedAt         time.Time `gorm:"autoUpdateTime"`
+}
+
+func FromDomain(u products.Domain) Products {
+	return Products{
+		ProductClassId:    u.ProductClassId,
+		ProductCategoryId: u.ProductCategoryId,
+		Name:              u.Name,
+		AdminFee:          u.AdminFee,
+		Stock:             u.Stock,
+		Price:             u.Price,
+		IsActive:          u.IsActive,
+	}
+}
+
+func (u *Products) ToDomain() products.Domain {
+	return products.Domain{
+		ID:                u.ID,
+		ProductClassId:    u.ProductClassId,
+		ProductCategoryId: u.ProductCategoryId,
+		Sku:               u.Sku,
+		Name:              u.Name,
+		AdminFee:          u.AdminFee,
+		Stock:             u.Stock,
+		Price:             u.Price,
+		IsActive:          u.IsActive,
+	}
+}
+
+func ToDomainList(user []Products) (list []products.Domain) {
+	for _, v := range user {
+		list = append(list, v.ToDomain())
+	}
+	return list
+}
